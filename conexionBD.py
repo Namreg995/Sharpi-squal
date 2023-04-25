a1b3deabc59b072e71418a80e76e511fb4b881fe
Namreg995 <111881836+Namreg995@users.noreply.github.com>
2023-04-25 17:18:20 -0300

    Delete conexionBD.py

--- conexionBD.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-conn = psycopg2.connect(
-   database="Sharpi-equal", user='postgres', password='Mnbvcxz531', host='127.0.0.1', port= '5432'
-)
-cur = conn.cursor()
-cur.execute("select current_database()")
-result = cur.fetchone()
-print(result)
-conn.close()
